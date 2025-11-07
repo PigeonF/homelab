dda1e8cb78fbdabfd5f25da27b92b20799ae96af
Jonas Fierlings <fnoegip@gmail.com>
2025-11-07 15:24:18 +0100

    Enable SSH access to the NixOs installer

--- installer/configuration.nix
+++ installer/configuration.nix
@@ -1,4 +1,22 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
 {
   imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+
+  networking = {
+    hostName = "nixos-installer";
+  };
+
+  users = {
+    users = {
+      # NOTE(PigeonF): Since this is just for the ephemeral installer we just hardcode the passwords.
+      nixos = {
+        initialHashedPassword = lib.mkForce null;
+        initialPassword = "nixos";
+      };
+      root = {
+        initialHashedPassword = lib.mkForce null;
+        initialPassword = "root";
+      };
+    };
+  };
 }
