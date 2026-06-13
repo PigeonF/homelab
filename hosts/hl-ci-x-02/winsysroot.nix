4c406ff034bec7c73f30fc8a837e04cc58285031
Jonas Fierlings <fnoegip@gmail.com>
2026-06-13 09:43:29 +0200

    Add timeouts and retry when building winsysroot
    Signed-off-by: Jonas Fierlings <fnoegip@gmail.com>

--- hosts/hl-ci-x-02/winsysroot.nix
+++ hosts/hl-ci-x-02/winsysroot.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation {
   buildCommand = ''
     runHook preBuild
 
-    xwin --accept-license --arch=x86 --arch=x86_64 --arch=aarch64 --sdk-version="${sdk_version}" --crt-version="${crt_version}" splat --copy --include-debug-libs --preserve-ms-arch-notation --use-winsysroot-style --output="$out"
+    xwin --timeout=120 --http-retry=3 --accept-license --arch=x86 --arch=x86_64 --arch=aarch64 --sdk-version="${sdk_version}" --crt-version="${crt_version}" splat --copy --include-debug-libs --preserve-ms-arch-notation --use-winsysroot-style --output="$out"
 
     runHook postBuild
   '';
