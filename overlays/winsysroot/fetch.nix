e6f97c3a427d42d8dfd449a11f8d417ecc965a38
Jonas Fierlings <fnoegip@gmail.com>
2026-06-27 09:31:41 +0200

    Update winsysroot
    Signed-off-by: Jonas Fierlings <fnoegip@gmail.com>

--- overlays/winsysroot/fetch.nix
+++ overlays/winsysroot/fetch.nix
@@ -20,7 +20,7 @@ stdenv.mkDerivation {
   buildCommand = ''
     runHook preBuild
 
-    xwin --timeout=120 --http-retry=3 --accept-license --arch=x86 --arch=x86_64 --arch=aarch64 --sdk-version="${sdk}" --crt-version="${crt}" splat --copy --include-debug-libs --preserve-ms-arch-notation --use-winsysroot-style --output="$out"
+    xwin --timeout=120 --http-retry=3 --accept-license --arch=x86 --arch=x86_64 --arch=aarch64 --sdk-version="${sdk}" --crt-version="${crt}" splat --copy --preserve-ms-arch-notation --output="$out"
 
     runHook postBuild
   '';
