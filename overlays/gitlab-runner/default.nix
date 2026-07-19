c83dc85d9df9b6939675fac286450659221f912e
Jonas Fierlings <fnoegip@gmail.com>
2026-07-19 14:27:04 +0200

    Update gitlab runner to 19.2
    Signed-off-by: Jonas Fierlings <fnoegip@gmail.com>

--- overlays/gitlab-runner/default.nix
+++ overlays/gitlab-runner/default.nix
@@ -1,9 +1,23 @@
-{ gitlab-runner, ... }:
+{
+  gitlab-runner,
+  fetchFromGitLab,
+  ...
+}:
 gitlab-runner.overrideAttrs (
-  _: previousAttrs: {
+  finalAttrs: previousAttrs: {
+    version = "19.2.0";
+
+    src = fetchFromGitLab {
+      owner = "gitlab-org";
+      repo = "gitlab-runner";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-aWX506nR7yPHWIXBQq/Mwu+EFoa4DYKCjKNT74A85xs=";
+    };
+
+    vendorHash = "sha256-kznAIKiJUZUkoNQSsnl6pj4SZspErccc6rmGwuc7iKo=";
+
     patches = (previousAttrs.patches or [ ]) ++ [
       ./services_cap_add.patch
-      # ./services_variables.patch
     ];
     doCheck = false;
   }
